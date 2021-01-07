305e2ca4fdfb5f99c2518e08ecb86d6bd4ac68b6
jisoo03010 <sdh20200305@sdh.hs.kr>
2021-01-07 21:16:36 +0900

    내가 만든 자기소개 페이지 ! (1학년 때 만듦)

---
+++ js/screen.js
@@ -0,0 +1,6 @@
+document.getElementById("folder").onclick = function(){
+    let folderpage = document.getElementById("folderpage")
+    if(folderpage.style.display == "none") folderpage.style.display = "block"
+    else folderpage.style.display = "none"
+    
+}
