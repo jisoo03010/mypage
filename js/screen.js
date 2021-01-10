f91904e40876f2f681ba20bb76a40a9e1c9f1abb
jisoo03010 <sdh20200305@sdh.hs.kr>
2021-01-10 14:58:21 +0900

    mypage

--- js/screen.js
+++ js/screen.js
@@ -4,3 +4,16 @@ document.getElementById("folder").onclick = function(){
     else folderpage.style.display = "none"
     
 }
+let blurback = document.getElementsByClassName("blurback")[0]
+function login(pwd){
+    if(pwd == "jisoo0310"){
+        blurback.animate([
+            {opacity: 0}
+        ],2000)
+        setTimeout(function(){
+            blurback.style.display = "none"
+        }, 2000)
+       
+        
+    }
+}
